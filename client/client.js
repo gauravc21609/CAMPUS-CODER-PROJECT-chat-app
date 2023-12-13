bf8d4d47e6e1b97aa30fcb59dff8156ee42690c6
gaurav <gchauhan21609@gmail.com>
2023-12-13 15:07:47 +0530

    client/package.json is delete and defer is added in script.

--- client/client.js
+++ client/client.js
@@ -4,5 +4,17 @@ const from = document.getElementById('send-container');
 const messageInput = document.getElementById('messageInp')
 const messageContainer = document.querySelector(".container")
 
+const append = (message, postion) =>{
+  const messageElement = document.createElement('div');
+  messageElement.innerHTML = message;
+  messageElement.classList.add('message')
+  messageElement.classList.add('position');
+  messageContainet.append('messageElement');
+}
+
 const name = prompt( "Enter your name to join");
 socket.emit('new-user-joined', name);
+
+Socket.on('user-joined', data =>{
+  append('${name} joined the Chat', 'right')
+})
