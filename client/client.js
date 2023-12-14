058332a60831ba18aee1b3160daecb98b8e37356
gaurav <gchauhan21609@gmail.com>
2023-12-14 15:35:58 +0530

    conncetion left

--- client/client.js
+++ client/client.js
@@ -3,18 +3,38 @@ const socket = io('http://localhost:8000');
 const from = document.getElementById('send-container');
 const messageInput = document.getElementById('messageInp')
 const messageContainer = document.querySelector(".container")
+var audio = new Audio('ding-idea.mp3');
 
 const append = (message, postion) =>{
   const messageElement = document.createElement('div');
   messageElement.innerHTML = message;
-  messageElement.classList.add('message')
+  messageElement.classList.add('message');
   messageElement.classList.add('position');
-  messageContainet.append('messageElement');
+  messageContainer.append('messageElement');
+  if(position == 'left'){
+    audio.play();
+  }
 }
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const message = messageInput.value;
+  append('you: ${message}', 'right');
+  socket.emit('send', message);
+  messageInput.value = ''
+})
 
 const name = prompt( "Enter your name to join");
 socket.emit('new-user-joined', name);
 
-Socket.on('user-joined', data =>{
+Socket.on('user-joined', name =>{
   append('${name} joined the Chat', 'right')
+})
+
+Socket.on('receive', data =>{
+  append('${data.name}: ${data.message}', 'left')
+})
+
+Socket.on('left', name =>{
+  append('${data.name} left the chat', 'left')
 })
--- nodeServer/index.js
+++ nodeServer/index.js
@@ -5,12 +5,17 @@ const users = {};
 
 io.on('connection', socket =>{
     socket.on('new-user-joined', name =>{
-      console.log("New user", name);
       users[socket.id] = name;
       socket.broadcast.emit('user-joined', name);
     });
 
     socket.on('send', message =>{
-       socket.broadcast.emit('recieve', {message : message, name: user[socket.id]})
+       socket.broadcast.emit('recieve', {message : message, name: users[socket.id]})
     });
-} )
+
+    socket.on('disconnect', message =>{
+      socket.broadcast.emit('left', users[socket.id]);
+      delete users[socket.id];
+    });
+});
+
